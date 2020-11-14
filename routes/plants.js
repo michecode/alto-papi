5126a63eb1031a88a27b3905736903794244401a
michecode <44727187+michecode@users.noreply.github.com>
2020-11-13 22:46:48 -0500

    didnt break lol

--- routes/plants.js
+++ routes/plants.js
@@ -104,7 +104,7 @@ const logger = winston.createLogger({
         Write all logs with level 'info' and below to 'combined.log'
         */
        new winston.transports.File({ filename: '/opt/logs/error.log', level: 'error' }),
-       new winston.transports.File({ filename: '/opt/logs/combined.log '}),
+       new winston.transports.File({ filename: '/opt/logs/combined.log'}),
     ],
 });
 
